013625061ccf56394c5872efa5b6b976b93246f2
Tiffany Gachuma <tiffany@Tiffanys-MacBook-Pro.local>
2025-11-17 13:00:59 +0000

    Added front-end whiteboard

---
+++ web/src/App.jsx
@@ -0,0 +1,12 @@
+import { Tldraw } from '@tldraw/tldraw'
+import '@tldraw/tldraw/tldraw.css'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Tldraw />
+    </div>
+  )
+}
+
+export default App
