8f2baaf6c3ef16b8c9bb6a98775bae2306c842f9
ma971 <ma971@student.le.ac.uk>
2025-12-02 15:55:27 +0000

    Fix TLDraw syncing and WebSocket state persistence

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,13 +1,15 @@
 import { Tldraw } from '@tldraw/tldraw'
 import '@tldraw/tldraw/tldraw.css'
+import { createWebSocketStore } from './wsStore'
 
 function App() {
+  const store = createWebSocketStore()
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <Tldraw persistenceKey="board-instance" />
+      <Tldraw store={store} />
     </div>
   )
 }
 
 export default App
-
---
+++ web/src/wsStore.js
@@ -0,0 +1,33 @@
+import { createTLStore, defaultShapeUtils, defaultAppState } from '@tldraw/tldraw'
+
+export function createWebSocketStore() {
+  const store = createTLStore({
+    shapeUtils: defaultShapeUtils,
+  })
+
+  const url = `ws://${window.location.hostname}:8080/ws`
+  const socket = new WebSocket(url)
+
+  socket.onopen = () => {
+    console.log("Connected to WebSocket server")
+  }
+
+  socket.onmessage = (event) => {
+    const msg = JSON.parse(event.data)
+
+    if (msg.type === "load") {
+      store.loadSnapshot(msg.data)
+      return
+    }
+
+    if (msg.type === "patch") {
+      store.patchState(msg.data)
+    }
+  }
+
+  store.onPatch((patch) => {
+    socket.send(JSON.stringify({ type: "patch", data: patch }))
+  })
+
+  return store
+}
